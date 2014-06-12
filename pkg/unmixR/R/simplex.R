0c82732e52c0e24385440a3205875c773aeacb16
Claudia Beleites <chemometrie@beleites.de>
2014-06-12 11:58:48 +0200

    rewrote .simplex: - got rid of old definition - added assertion for corresponding size of data and indices - simplify working code   column selection can be due to assertion - added test

--- pkg/unmixR/R/simplex.R
+++ pkg/unmixR/R/simplex.R
@@ -14,38 +14,42 @@
 ##' @rdname simplex
 ##' @include unmixR-package.R
 
-# .simplex <- function(data, p, indices) {
-  # rbind(rep(1, p), t(data[indices,]))
-# }
-
 # The following is Bryan's modification so that the function does what
 # it is claimed to do, return a p x p matrix
 # This was not really necessary, but does not harm; I was forgetting that
 # nfinder.default was doing the PCA before coming here.
 
+## TODO: @CB check why p is needed at all: it should be clear from both length
+## (indices) and ncol (data)
+## TODO: @CB check wheter data columns 1 : (p - 1) is needed
+
 .simplex <- function(data, p, indices) {
-  if (length(indices) != p) {
-  	stop("indices should have length p")
+  if (length (indices) != p) {
+  	stop("p indices needed to form simplex")
   }
-  data <- data[indices,]
-  tdata <- t(data)
-  tdata <- tdata[1:(p-1),]
-  rbind(rep(1, p), tdata)
+
+  if (ncol (data) != length (indices) - 1L)
+    stop ("length (indices) does not correspond to dimensionality of data")
+  
+  data <- data [indices, , drop = FALSE]
+  rbind (rep (1, p), t (data))
 }
 
 .test(.simplex) <- function() {
-  # test: verify the output of .simplex
+  checkException (.simplex (data, 2, indices))
+  checkException (.simplex (data [, rep (1, 3)], 3, indices))
   
   p <- 3
   rows <- 5
   indices <- c(1, 3, 5)
   
-  data <- matrix(1:((p-1)*rows), ncol=p-1, nrow=rows, byrow=TRUE)
-  expected <- matrix(c(
-    c(1,1,1),
-    c(1,5,9),
-    c(2,6,10)
-  ), ncol=p, byrow=TRUE)
+  data <- matrix (seq_len ((p - 1) * rows), 
+                  ncol = p - 1, nrow = rows, byrow = TRUE)
   
-  checkEquals(expected, .simplex(data, p, indices))
+  expected <- matrix(c (1, 1,  1,
+                        1, 5,  9, 
+                        2, 6, 10), ncol = p, byrow = TRUE)
+  
+  checkEquals (expected, .simplex (data, p, indices))
+  
 }
