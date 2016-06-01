bd9c5716506d4b8dcd38ba48568a3c1e90321c37
AaBelov <vip.a.belov@mail.ru>
2016-06-01 23:51:12 +0300

    forgot to push actual tests

--- pkg/unmixR/tests/testthat/test_VCA.R
+++ pkg/unmixR/tests/testthat/test_VCA.R
@@ -5,3 +5,21 @@ test_that("VCA works on test data", {
     expect_true(all(indices %in% correct))
     expect_false(any(duplicated(indices)))
 })
+
+test_that("VCA works on test data", {
+    indices <- vcaLopezFromScratch(testData, 2)
+    expect_true(all(indices %in% correct))
+    expect_false(any(duplicated(indices)))
+})
+
+test_that("VCA works on test data", {
+    indices <- vcaFromScratch(testData, 3)
+    expect_true(all(indices %in% correct))
+    expect_false(any(duplicated(indices)))
+})
+
+test_that("VCA works on test data", {
+    indices <- vcaLopezFromScratch(testData, 3)
+    expect_true(all(indices %in% correct))
+    expect_false(any(duplicated(indices)))
+})
