8372dacd68e29e2ba4d9e72a892e7916258273fa
AaBelov <vip.a.belov@mail.ru>
2016-06-27 16:38:18 +0300

    renamed Mod and Lopez methods

--- pkg/unmixR/R/vcaMod.R
+++ pkg/unmixR/R/vcaMod.R
@@ -1,72 +1,56 @@
-##' Modified Vertex Component Analysis
+##' J. F. Lopez Vertex Component Analysis
 ##'
-##' Modified VCA algorithm that aims to reduced the algorithmic complexity of
-##' the original.
-##' Intended to be called from \code{\link{nfindr}}.
+##' The original VCA algorithm (\code{\link{vca}}) contains auxiliary
+##' operations (signal to noise estimation, etc.) which could be omitted while
+##' still maintaining a fully functional algorithm. In the paper that describes
+##' the Modified VCA algorithm (\code{\link{vcaMod}}) the author provides a
+##' description of the original VCA with only the essential components
+##' included. This is an implementation of that algorithm
+##' Intended to be called from \code{\link{vca}}.
 ##' 
 ##' @param data Data matrix.
 ##'
 ##' @param p Number of endmembers.
 ##'
-##' @return The indices of the endmembers in the original dataset.
+##' @return Indices of the endmembers in the original dataset.
 ##' 
-##' @references Lopez, S., Horstrand, P., Callico, G.M., Lopez J.F. and
+##' @references Lopez, S., Horstrand, P., Callico, G.M., Lopez, J.F. and
 ##' Sarmiento, R., "A Low-Computational-Complexity Algorithm for
 ##' Hyperspectral Endmember Extraction: Modified Vertex Component Analysis,"
 ##' Geoscience & Remote Sensing Letters, IEEE, vol. 9 no. 3 pp. 502-506, May 2012
 ##' doi: 10.1109/LGRS.2011.2172771
 ##'
 ##' @export
-##' @importFrom stats prcomp
+##' @importFrom stats prcomp runif
 
+vcaLopez <- function(data, p) {
+  data <- as.matrix(data)
+  Y <- t(stats::prcomp(data)[["x"]][,1:p])
 
-vcaMod <- function(data, p) {
-    data <- t(as.matrix(data))
-    
-    Y <- dimensionalityReduction(data, p, estSNR(data, p))
-    
-    #matrix of endmembers
-    E <- matrix(0, nrow = p, ncol = p + 1)
-    E[p, 1] <- 1
-    #U stores the set of orthogonal vectors
-    U <- matrix(0, nrow = p, ncol = p)
-    #px1 vector
-    w <- c(rep(1, p))
-    proj_acc <- c(rep(0, p))
-    indices <- c(rep(0, p))
-    for (i in 1:p) {
-        #U_i is initialized with the endmember computed in the last iteration
-        U[, i] <- E[, i]
-        
-        #Gram-Schmidt orthogonalization
-        if(i >= 3)
-        {
-            for (j in 3:i) {
-                proj_ei_uj_1 <- (crossprod(E[, i], U[, j - 1]) / crossprod(U[, j - 1])) * U[, j - 1]
-                U[, i] <- U[, i] - proj_ei_uj_1
-            }
-        }
-        #U_i is orthogonal to other i-1 vectors
-        
-        #projecting w onto U_i
-        proj_w_ui <- (crossprod(w, U[, i]) / crossprod(U[, i])) * U[, i]
-        
-        #vector f is orthogonal to to the subspace spaned by columns of E
-        proj_acc <- proj_acc + proj_w_ui
-        f <- w - proj_acc
-        
-        #projection accumulator is reset on the first iteration
-        if(i == 1){
-            proj_acc <- c(rep(0, p))
-        }
-        #projecting data onto f
-        v <- crossprod(f, Y)
-        #getting index of the maximal projection
-        index <- which.max(abs(v))
-        indices[i] <- index
-        #estimated endmember is stored in E
-        E[, i + 1] <- Y[, index]
+  E <- matrix(0, nrow=p, ncol=p)
+  E[p,1] <- 1
+  I <- diag(p)
+
+  indices <- array(0, p)
+
+  for (i in 1:p) {
+    w <- runif(p)
+    x <- (I - (E %*% ginv(E))) %*% w
+    f <- x / sqrt(sum(x^2))
+    v <- crossprod(f, Y)
+
+    index <- which.max(abs(v))
+
+    if (.options ("debuglevel") >= 1L){
+      print (which.max (v))
+      print (which.min (v))
     }
-    indices <- sort(indices)
-    indices
+
+    indices[i] <- index
+    E[, i] <- Y[, index]
+  }
+
+  indices <- sort(indices)
+
+  indices
 }
