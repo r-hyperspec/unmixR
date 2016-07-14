a9b5970f6ba6ed0bd42a74383d6cdc30f59024cd
Claudia Beleites <chemometrie@beleites.de>
2016-07-14 11:57:12 +0200

    bugfix + housekeeping nfindr.formula: - make sure no intercept term is produced - stop on response

--- pkg/unmixR/R/nfindr.formula.R
+++ pkg/unmixR/R/nfindr.formula.R
@@ -2,13 +2,19 @@
 ##' @rdname nfindr
 ##' @export
 ##' @include nfindr.default.R
-##' @importFrom stats model.matrix
+##' @importFrom stats model.matrix terms
 ##'
-nfindr.formula <- function(formula, frame, p,
-                           method="LDU", indices = sample (nrow (data), p), ...,
-                           drop=FALSE) {
+nfindr.formula <- function(formula, frame, ...) {
+  mt <- terms(formula, data = frame)
+  
+  ## response term is not allowed
+  if (attr(mt, "response") > 0L) stop("N-FINDR models cannot have response")
+  
+  ## drop intercept silently
+  attr(mt, "intercept") <- 0L
+  
+  data <- model.matrix (mt, frame)
 
-  data <- stats::model.matrix (formula, frame)
-
-  nfindr (data, p, method, indices, ..., drop=drop)
+  nfindr (data, ...)
 }
+
