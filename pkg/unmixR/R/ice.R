45ddcff842b9dd48c273130c2947ae31624b415b
AaBelov <vip.a.belov@mail.ru>
2016-07-24 23:55:42 +0300

    removed default p commented code

--- pkg/unmixR/R/ice.R
+++ pkg/unmixR/R/ice.R
@@ -3,6 +3,7 @@
 ##'@export
 
 ice <- function(data, p, mu = 0.01, t = 0.9){
+
     data <- t(as.matrix(data))
     curEnd <- data[, sample(dim(data)[2], p)]
     
@@ -14,7 +15,7 @@ ice <- function(data, p, mu = 0.01, t = 0.9){
         abund <- apply(data, 2, function(spectrum){
             limSolve::lsei(A = curEnd, B = spectrum, E = rep(1, p), F = 1, diag(1, p), rep(0, p))$X
         })
-        
+
         lambda <- dim(data)[1] * mu / ((p - 1)*(1 - mu))
         
         curEndTransposed <- solve(abund %*% t(abund) + lambda*(diag(p) - 1/p * matrix(rep(1, p*p), nrow = p))) %*% (abund %*% t(data))
@@ -23,7 +24,7 @@ ice <- function(data, p, mu = 0.01, t = 0.9){
         v <- matrixcalc::matrix.trace(cov(curEnd))
         t_i_1 <- t_i
         t_i <- (1 - mu)/dim(data)[1]*matrixcalc::frobenius.norm(data - curEnd %*% abund) + mu*v
-        
+
         if(nIter > 1){
             if (t_i / t_i_1 >= t) {
                 break
