6fee9a0667acb5aa8904eae415b75591739b5d7a
AaBelov <vip.a.belov@mail.ru>
2016-08-11 03:21:55 +0300

    added tau info, corrected ICE default values

--- pkg/unmixR/R/ice.R
+++ pkg/unmixR/R/ice.R
@@ -33,7 +33,7 @@
 ##' @importFrom limSolve lsei
 ##' @importFrom matrixcalc matrix.trace frobenius.norm
 ##'
-ice <- function(data, p, mu = 0.01, t = 0.99){
+ice <- function(data, p, mu = 0.00001, t = 0.9999){
 
     data <- t(as.matrix(data))
     #selecting several points as initial endmembers
