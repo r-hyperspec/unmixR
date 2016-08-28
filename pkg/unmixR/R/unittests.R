141c5dc9f968797d360b813783c4e8983f106add
Claudia Beleites <chemometrie@beleites.de>
2016-08-28 14:36:23 +0200

    get rid of `testthat:::`

--- pkg/unmixR/R/unittests.R
+++ pkg/unmixR/R/unittests.R
@@ -12,12 +12,12 @@
 ##' @keywords programming utilities
 ##' @export
 ##' @include unmixR-package.R
+##' @importFrom testthat SummaryReporter ListReporter MultiReporter get_reporter
+##' 
 
-unmixR.unittest <- function (reporter = "summary") {
-  
-  # test_check("unmixR")
-  
-  if (!requireNamespace("testthat", quietly=TRUE)) {
+unmixR.unittest <- function () {
+
+    if (!requireNamespace("testthat", quietly=TRUE)) {
     warning("testthat required to run the unit tests.")
     return(NA)
   }
@@ -25,19 +25,19 @@ unmixR.unittest <- function (reporter = "summary") {
   tests <- eapply(env = getNamespace ("unmixR"), FUN = get.test, all.names=TRUE)
   tests <- tests [! sapply (tests, is.null)]
   
-  reporter <- testthat:::find_reporter(reporter)
-  lister <- testthat:::ListReporter$new()
+  reporter <- SummaryReporter$new()
+  lister <- ListReporter$new()
   reporter <- MultiReporter$new(reporters = list(reporter, lister))
-  
+
   with_reporter(reporter = reporter, start_end_reporter = TRUE, {
     for (t in seq_along(tests)){
       lister$start_file(names (tests [t]))
       tests [[t]] ()
     }
-    testthat:::end_context()
+    get_reporter()$.end_context()
   })
 
-  
+
  invisible(lister$get_results())
 }
 
