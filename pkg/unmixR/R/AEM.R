dccb4488785b6127640a05e56f27a3f6da873780
Bryan Hanson <hanson@depauw.edu>
2017-06-21 20:21:47 -0400

    improvements to AEM, formerly plotAmap

--- pkg/unmixR/R/AEM.R
+++ pkg/unmixR/R/AEM.R
@@ -1,8 +1,8 @@
-##' Plot an Abundance Map Based on Endmembers
+##' Abundance Map and Endmembers
 ##' 
 ##' Convenience function to take a \code{hyperSpec} object, and the results
 ##' of unmixing that object, and create an abundance map using the
-##' specified endmembers.
+##' specified endmembers.  The map data and the individual endmembers are returned.
 ##' 
 ##' @param hS An object of class \code{hyperSpec}.
 ##'
@@ -11,38 +11,32 @@
 ##'
 ##' @param EMs Integer.  The endmembers to include in the map.
 ##'
-##' @param ret Character.  One of \code{c("Amap", "EM")}.  In the first case, return
-##'        the abundance map data.  In the second case, return the endmembers.  In
-##'        both cases a \code{hyperSpec} object is returned invisibly.
-##'
-##' @param plot Logical.  Shall the abundance map be plotted?
+##' @param plotMap Logical.  Shall the abundance map be plotted?
 ##'
 ##' @param ... Additional parameters to be passed to levelplot/hyperSpec::plotmap.
 ##' 
-##' @return A list of class \code{vca} which contains:
+##' @return A list with two \code{hyperSpec} objects:
 ##'   \itemize{
-##'     \item \strong{data}: Either the original data or just the endmembers if
-##'                          \code{EMonly = TRUE}.
-##'     \item \strong{indices}: The indices the endmembers in the suppled \code{data}.
-##'     \item \strong{seed}: The value of \code{seed} if \code{debuglevel >= 1}. 
+##'     \item \strong{Endmembers}: The endmembers
+##'     \item \strong{Map}: The map constructed from the specified endmembers.
 ##'   }
 ##' 
-##' @seealso \code{\link{endmembers}} to extract the spectra; \code{\link{predict}}
-##' to determine abundances of endmembers in each sample.
-##'
 ##' @export
 ##' @include unmixR-package.R
 ##'
 ##' @examples
 ##' unmix <- vca(chondro, p = 3, method = "Lopez2012")
-##' plotAmap(chondro, unmix, EMs = 1:3)
-##' 
+##' aem <- AEM(chondro, unmix, EMs = 1:3) # plot the map
+##' # Show the endmembers
+##' plotspc(aem[[1]], stacked = TRUE, col = c("red", "green", "blue"))
+##'
 
-plotAmap <- function(hS, uM, EMs = 1, ret = "Amap", plot = TRUE, ...) {
+
+AEM <- function(hS, uM, EMs = 1, plotMap = TRUE, ...) {
 
 
   if (length(EMs) > length(uM$indices)) {
-    msg <- "More endmembers requested than exist.\n\tCheck length(EMs) & the value of p used in unmixing." 
+    msg <- "More endmembers requested than exist.\n\tCheck length(EMs) & the value of p used for unmixing." 
     stop(msg) 	
   }
   
@@ -55,20 +49,21 @@ plotAmap <- function(hS, uM, EMs = 1, ret = "Amap", plot = TRUE, ...) {
 	# Remove all the spectra except leave enough rows for the endmembers
 	EM <- EM[-c((no.em + 1):no.spc)]
 	EM[[]] <- endmembers(uM) # extract and insert endmembers
-
+	# Add extra info marking the endmembers to facilitate plot conditioning
+	EM$em <- as.factor(LETTERS[1:no.em])
+	
 	# Build abundance map data
 	abun <- .predict(uM)
 	map <- abun[,EMs] %*% uM$data[uM$indices[EMs],, drop = FALSE]
 	MAP <- hS # make a copy to hold abundance map
 	MAP[[]] <- map
-	if (plot) p <- hyperSpec::plotmap(MAP, ...); print(p)	
+	if (plotMap) p <- hyperSpec::plotmap(MAP, ...); print(p)	
   }
 
   if (class(uM) == "ice") {
   	stop("mapping ice objects not yet implemented")
   }
 
-  if (ret == "Amap") ans <- MAP
-  if (ret == "EM") ans <- EM
+  ans <- list(Endmembers = EM, Map = MAP)
   invisible(ans)
 }
